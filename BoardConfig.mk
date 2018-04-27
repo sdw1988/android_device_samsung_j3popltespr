fb745de74dfa1d7f923c070f64a96f365dd25441
sleepychan <sleepychan@users.noreply.github.com>
2018-04-26 18:21:21 -0700

    whoops

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,11 @@ TARGET_NO_RPC := true
 
 TARGET_BOARD_PLATFORM := msm8937
 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_CPU_SMP := true
